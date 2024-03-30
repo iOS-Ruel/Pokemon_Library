f7a825b2aa86710ac7a603638bb2bbc321b42f28
Ruel <c0407h@gmail.com>
2024-03-30 14:27:51 +0900

    Initial Commit

---
+++ Pokemon_Library/Pokemon_LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_LibraryApp.swift
+//  Pokemon_Library
+//
+//  Created by Chung Wussup on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
